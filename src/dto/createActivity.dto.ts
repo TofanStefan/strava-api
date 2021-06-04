57212ffceef4f6afb109293bda219a29e5540212
Tofan Stefan <tofanstefan99@gmail.com>
2021-06-04 13:25:57 +0300

    ABC-011

--- src/dto/createActivity.dto.ts
+++ src/dto/createActivity.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate, IsDateString, IsNotEmpty, IsString } from 'class-validator';
+import { IsDate, IsDateString, IsIn, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 export class CreateActivityDto{
     @IsNotEmpty() @IsString()
     name: string;
@@ -9,14 +9,18 @@ export class CreateActivityDto{
     @IsNotEmpty() @IsDateString()
     start_date_local: Date;
 
-    @IsNotEmpty()
+    @IsNotEmpty() @IsInt()
     elapsed_time: number;
 
+    @IsOptional() @IsString()
     description?: string;
 
+    @IsOptional() @IsNumber()
     distance?: number;
 
+    @IsOptional() @IsInt()
     trainer?: number;
 
+    @IsOptional() @IsInt()
     commute?: number;
 }
